a116a969d26eb199a2439e5dfb95545f91a2700c
Corneliu CROITORU <cornel.croitoru@gmail.com>
2023-01-31 10:00:30 +0100

    first commit - working code

---
+++ extensions/WaitTimes/lambda/average/index.js
@@ -0,0 +1,84 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const getDrinkByOrderId = async (orderId) => {
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        Key: {
+            orderId: orderId,
+        },
+    };
+
+    console.log('getDrinkByOrderId params: ', params)
+
+    try {
+        const result = await dynamoDb.get(params).promise();
+        console.log('result: ', result.Item)
+        return result.Item.drink;
+    } catch (err) {
+        console.error('getItem error: ', err)
+    }
+}
+
+const getAllOrderByDrink = async (drink) => {
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        IndexName: process.env.GSI_NAME,
+        KeyConditionExpression: 'drink = :drink',
+        ExpressionAttributeValues: {
+            ':drink': drink,
+        },
+        FilterExpression: 'attribute_exists(drink_order_completed)',
+        ProjectionExpression: 'orderId, drink, drink_order_submitted, drink_order_completed',
+    };
+
+    console.log('getDrinkByOrderId params: ', params)
+
+    try {
+        const result = await dynamoDb.query(params).promise();
+        return result.Items;
+    } catch (err) {
+        console.error('getItem error: ', err)
+    }
+}
+
+const calculateAverageTimes = (orders, resultsNb) => {
+
+    // Sort the results by drink_order_completed in descending order
+    orders.sort((a, b) => new Date(b.drink_order_completed) - new Date(a.drink_order_completed));
+
+    // Get the most 10 recent items
+    const mostRecentItems = orders.slice(0, resultsNb);
+
+    // Calculate the total duration of the most 10 recent items
+    let totalDuration = 0;
+    for (let item of mostRecentItems) {
+        const submitted = new Date(item.drink_order_submitted);
+        const completed = new Date(item.drink_order_completed);
+        totalDuration += completed - submitted;
+    }
+
+    // Calculate the average duration of the most 10 recent items
+    const averageDuration = totalDuration / mostRecentItems.length / 1000;
+    console.log('Average duration (seconds):', averageDuration);
+    return averageDuration;
+
+}
+
+
+exports.handler = async (event, context) => {
+    console.log('event', JSON.stringify(event));
+    const drink = await getDrinkByOrderId(event.detail.orderId);
+    console.log('drink', drink);
+    const orders = await getAllOrderByDrink(drink);
+    console.log('orders', JSON.stringify(orders)); 
+    const duration = calculateAverageTimes(orders, parseInt(process.env.NB_ITEMS));
+    console.log("duration", duration);
+    const response = {
+        drink: drink,
+        averageDuration: duration,
+        source: process.env.SOURCE
+    };
+    console.log('response', JSON.stringify(response));
+    return response;
+};
---
+++ extensions/WaitTimes/lib/wait_times_stack.ts
@@ -0,0 +1,157 @@
+import {
+  Aws,
+  Duration,
+  Stack,
+  StackProps,
+  aws_stepfunctions as sfn,
+  aws_stepfunctions_tasks as tasks,
+  aws_logs as logs,
+  aws_lambda as lambda,
+  aws_events as events,
+  aws_events_targets as targets,
+  aws_dynamodb as dynamodb,
+  aws_sqs as sqs,
+  CfnOutput,
+} from "aws-cdk-lib";
+import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { Construct } from 'constructs';
+
+export class WaitTimesStack extends Stack {
+
+  public readonly busName = "Serverlesspresso";
+  public readonly source = "awsserverlessda.serverlesspresso";
+  public readonly detailTypeSubmitted = "OrderManager.WaitingCompletion";
+  public readonly detailTypeCompleted = "OrderProcessor.orderFinished";
+  public readonly detailTypeAverageTime = "OrderManager.DrinkAverageTime";
+  public readonly nbItemsForAverage = "10";
+  
+
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const eventBridgeBus = events.EventBus.fromEventBusName(this, 'eventBus', this.busName);
+
+    const ruleOrderSubmitted = new events.Rule(this, 'RuleSubmitted', {
+      eventPattern: {
+        source: [this.source],
+        detailType: [this.detailTypeSubmitted],
+      },
+      eventBus: eventBridgeBus
+    });
+
+    const ruleOrderCompleted = new events.Rule(this, 'RuleCompleted', {
+      eventPattern: {
+        source: [this.source],
+        detailType: [this.detailTypeCompleted],
+      },
+      eventBus: eventBridgeBus
+    });
+
+
+
+    const orderDurationTable = new dynamodb.Table(this, 'OrderDurationTable', {
+      partitionKey: {
+        name: 'orderId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+
+    orderDurationTable.addGlobalSecondaryIndex({
+      indexName: "drinkIndex",
+      partitionKey: {
+        name: "drink",
+        type: dynamodb.AttributeType.STRING,
+      },
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+
+
+    const saveSubmittedTime = new tasks.DynamoPutItem(this, 'Save Order Submitted Time', {
+      item: {
+        orderId: tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.detail.orderId')),
+        drink: tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.detail.drinkOrder.drink')),
+        drink_order_submitted: tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.time')),
+      },
+      table: orderDurationTable,
+    });
+
+    const saveCompletedTime = new tasks.DynamoUpdateItem(this, 'Save Order Completed Time', {
+      key: {
+        orderId: tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.detail.orderId')),
+      },
+      table: orderDurationTable,
+      expressionAttributeValues: {
+        ':drink_order_completed': tasks.DynamoAttributeValue.fromString(sfn.JsonPath.stringAt('$.time')),
+      },
+      updateExpression: 'SET drink_order_completed = :drink_order_completed',
+      resultPath: sfn.JsonPath.DISCARD
+    });
+
+
+    const calculateAvgFunction = new lambda.Function(this, 'Function', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/average'),
+      environment: {
+        TABLE_NAME: orderDurationTable.tableName,
+        GSI_NAME: "drinkIndex",
+        NB_ITEMS: this.nbItemsForAverage,
+        SOURCE: this.source
+      },
+      tracing: lambda.Tracing.ACTIVE,
+    });
+
+    orderDurationTable.grantReadData(calculateAvgFunction);
+
+    const calculateAverageTime = new LambdaInvoke(this, 'Calculate Average Time', {
+      lambdaFunction: calculateAvgFunction,
+      invocationType: tasks.LambdaInvocationType.REQUEST_RESPONSE,
+      resultSelector: { "result.$": "$.Payload" },
+      outputPath: "$.result",
+
+    });
+
+    const emitEvent = new tasks.EventBridgePutEvents(this, 'Emit event', {
+      entries: [{
+        detail: sfn.TaskInput.fromObject({
+          drink: sfn.JsonPath.stringAt('$.drink'),
+          duration: sfn.JsonPath.stringAt('$.averageDuration'),
+        }),
+        eventBus: eventBridgeBus,
+        detailType: this.detailTypeAverageTime,
+        source: sfn.JsonPath.stringAt('$.source')
+      }],
+    });
+
+
+    const choice = new sfn.Choice(this, "Drink Order Submitted or Completed ?")
+      .when(
+        sfn.Condition.stringEquals("$.detail-type", this.detailTypeSubmitted),
+        saveSubmittedTime
+      )
+      .when(
+        sfn.Condition.stringEquals("$.detail-type", this.detailTypeCompleted),
+        saveCompletedTime.next(calculateAverageTime).next(emitEvent)
+      );
+
+
+    const durationSF = new sfn.StateMachine(this, "Duration",
+      {
+        definition: choice,
+        tracingEnabled : true
+      }
+    );
+
+    ruleOrderSubmitted.addTarget(new targets.SfnStateMachine(durationSF));
+    ruleOrderCompleted.addTarget(new targets.SfnStateMachine(durationSF));
+
+    new CfnOutput(this, 'StepFunctionName', { value: durationSF.stateMachineName });
+    new CfnOutput(this, 'LambdaFunctionName', { value: calculateAvgFunction.functionName });
+    new CfnOutput(this, 'DynamoDBTableName', { value: orderDurationTable.tableName }); 
+
+
+  }
+}
